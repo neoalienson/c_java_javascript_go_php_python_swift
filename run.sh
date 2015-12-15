d732f543b698e7b49a1b618914c065c845f92c7c
Neo Alienson <neo@orz.hk>
2015-12-15 12:02:30 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-go test -bench=. -count 10
+# go first
+echo "Sart test for Go"
+go test -bench=. -count 1
+
+# swift next
+echo "Compiling swift"
+swiftc benchmark_test.swift
+
+echo "Sart test for Swift"
+time -p ./benchmark_test
+rm benchmark_test
