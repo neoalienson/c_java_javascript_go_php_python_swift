cbb8344d59b418610d6c473193f416d962fe12f4
Neo Alienson <neo@orz.hk>
2015-12-15 14:35:22 +0800

    add all test

--- run.sh
+++ run.sh
@@ -1,36 +1,63 @@
 #!/bin/bash
 
+TIME='/usr/bin/time -f %E'
+HR='\n---------------------------------'
+
 # go first
+echo $HR
 echo "Start test for Go"
+mv benchmark_test.c benchmark_test.cx
 go test -bench=. -count 1
+go test -bench=. -count 1
+go test -bench=. -count 1
+mv benchmark_test.cx benchmark_test.c
 
 # swift next
-echo "Compiling swift"
+echo $HR
+echo "Start test swift"
 swiftc benchmark_test.swift
-
-echo "Start test for Swift"
-time -p ./benchmark_test
+$TIME ./benchmark_test
+$TIME ./benchmark_test
+$TIME ./benchmark_test
 rm benchmark_test
+
+# java
+java -version
+javac benchmark_test.java 
+$TIME java Benchmark
+$TIME java Benchmark
+$TIME java Benchmark
+rm Benchmark.class
 
 # javascript
 nodejs --version
-time nodejs benchmark_test.js
+$TIME nodejs benchmark_test.js
+$TIME nodejs benchmark_test.js
+$TIME nodejs benchmark_test.js
 
 # time for C
 echo "Start test for C (gcc)"
-gcc benchmark_test
-time -p ./a.out
+gcc benchmark_test.c
+$TIME ./a.out
+$TIME ./a.out
+$TIME ./a.out
 rm a.out
 
 echo "Start test for C (clang)"
-clang benchmark_test
-time -p ./a.out
+clang benchmark_test.c
+$TIME ./a.out
+$TIME ./a.out
+$TIME ./a.out
 rm a.out
 
 echo "Start test for PHP"
 php --version
-time -p php benchmark_test.php
+$TIME php benchmark_test.php
+$TIME php benchmark_test.php
+$TIME php benchmark_test.php
 
 echo "Start test for Python"
 python --version
-time -p python benchmark_test.py
+$TIME python benchmark_test.py
+$TIME python benchmark_test.py
+$TIME python benchmark_test.py
