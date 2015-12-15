2d1034a80a0be845aac1bdfa36c17ab0137caf4e
Neo Alienson <neo@orz.hk>
2015-12-15 13:43:35 +0800

    update script

--- run.sh
+++ run.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
 # go first
-echo "Sart test for Go"
+echo "Start test for Go"
 go test -bench=. -count 1
 
 # swift next
 echo "Compiling swift"
 swiftc benchmark_test.swift
 
-echo "Sart test for Swift"
+echo "Start test for Swift"
 time -p ./benchmark_test
 rm benchmark_test
+
+# javascript
+nodejs --version
+time nodejs benchmark_test.js
+
+# time for C
+echo "Start test for C (gcc)"
+gcc benchmark_test
+time -p ./a.out
+rm a.out
+
+echo "Start test for C (clang)"
+clang benchmark_test
+time -p ./a.out
+rm a.out
+
+echo "Start test for PHP"
+php --version
+time -p php benchmark_test.php
+
+echo "Start test for Python"
+python --version
+time -p python benchmark_test.py
